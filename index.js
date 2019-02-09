27bd4f2115f4b03889b2fcdc71589a09942d515e
kavya <soldatargent@gmail.com>
2019-02-09 18:36:27 +0530

    Server now reverses input

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get("*", (req, res) => {
-  res.send(req.url.slice(1))
+  res.send(req.url.slice(1).split("").reverse().join(""))
 });
 
 app.listen(port, function(){
