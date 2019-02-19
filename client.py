87e74da95386a73f3090637d0974ae2019d685fc
kavya <soldatargent@gmail.com>
2019-02-19 18:19:05 +0530

    changed index.js to be more computationally intensive (hopefully)

--- client.py
+++ client.py
@@ -1,6 +1,33 @@
 import requests
+import time
+import sys
+import random
+from multiprocessing import Process, Value
 
-url = 'http://127.0.0.1:3000/'
+# url = 'http://34.95.72.238:8080/'
+url = 'http://localhost:3000/'
 
-resp = requests.get(url + 'HelloWorld')
-print(resp.text)
+N_REQUESTS = int(sys.argv[1])
+
+def send_request():
+    while 1:
+        try:
+            resp = requests.get(url + str(random.randint(20, 60)))
+            break
+        except requests.exceptions.ConnectionError:
+            continue
+
+threads = []
+
+start = time.time()
+for i in range(N_REQUESTS):
+    t = Process(target=send_request)
+    threads.append(t)
+    t.start()
+
+for thread in threads:
+    thread.join()
+
+end = time.time()
+
+print("{0} {1}".format(N_REQUESTS, (end-start)))
--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const app = express()
 const port = 3000
 
 app.get("*", (req, res) => {
-  res.send(req.url.slice(1).split("").reverse().join(""))
+  ans = 1;
+  n = parseInt(req.url.slice(1))
+  for (var i = 2; i <= n; i++) {
+	ans = ans * i;
+  }
+  console.log(ans)
+  res.send(toString(ans))
 });
 
 app.listen(port, function(){
